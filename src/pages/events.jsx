3197256d55b539d909a0f16f6fce9cffed6889e7
Elena <eizquierdograu@gmail.com>
2019-07-07 23:25:14 +0200

    add some fixes but events not finished

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -22,13 +22,11 @@ import { generateRows } from '../demo-data/generator';
 
 const RowDetail = ({ row }) => (
     <div>
-        Details for
-        {' '}
-        {row.name}
-        {' '}
-        from
-        {' '}
-        {row.city}
+        Dilluns
+        Dimarts
+        Dimecres
+        Dijous
+        Divendres
     </div>
 );
 
@@ -39,10 +37,10 @@ class Events extends React.Component{
 
         this.state = {
             columns: [
-                { name: 'name', title: 'Name' },
-                { name: 'sex', title: 'Sex' },
-                { name: 'city', title: 'City' },
-                { name: 'car', title: 'Car' },
+                { name: 'setmana', title: 'Setmana' },
+                { name: 'valoracions', title: 'Valoració mitjana' },
+                { name: 'assistencia_voluntaris', title: 'Assistencia Voluntaris' },
+                { name: 'assistencia_infants', title: 'Assistencia Infants' },
             ],
             rows: generateRows({ length: 8 }),
         };
