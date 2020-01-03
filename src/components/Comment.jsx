932ff3772f72b12701354f8ad02e521a47bff16f
Elena <eizquierdograu@gmail.com>
2020-01-03 19:27:27 +0100

    improve View Topic style

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -8,15 +8,14 @@ import Moment from 'react-moment';
 class Comment extends React.Component {
     render(){
         return(
-            <div style={{marginBottom: "3%"}}>
-                <Row className="rowIcon2Style">
-                    <i className="fa fa-user iconStyle"></i>
-                    <h5> {this.props.user}</h5>
+            <div className="div-comment">
+                <Row>   
+                    <h5 style={{marginLeft: '1%'}}> {this.props.user}</h5>
                 </Row>
                 <Row style={{marginLeft: "3%"}}>
-                    <p>{this.props.content}</p>
+                    <p className="text-grey-style">{this.props.content}</p>
                 </Row>
-                <Moment style={{paddingLeft: "3%"}} format="DD/MM/YYYY HH:mm">{this.props.created_date}</Moment> 
+                <Moment className="text-grey-style" style={{paddingLeft: "3%"}} format="DD/MM/YYYY HH:mm">{this.props.created_date}</Moment> 
             </div>
         )
     }
